a6c65168e3e6db8353b372d416e287df80882e90
Kari Norman <kari@normanfamily.org>
2018-05-30 08:18:27 -0600

    Import trait data.

--- bbsAnalysis.R
+++ bbsAnalysis.R
@@ -1,3 +1,4 @@
 library(tidyverse)
 
-bird_trait <- read_csv("~/Documents/functional_diversity/data/elton_traits/elton_traits_BirdFuncDat.csv")
+bird_trait <- read_csv("~/Documents/Projects/functional_diversity/data/elton_traits/elton_traits_BirdFuncDat.csv")
+bbs <- read_csv('~/Documents/Projects/reserve_selection/data/mapping_data/bbs_species_2016.csv')
