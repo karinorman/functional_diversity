726d0b0bd34b8750c9fbc4c4fd38fd5141010258
Norman <kn0@macwf03125.ornl.gov>
2018-06-07 10:11:48 -0400

    Get FD metrics, assign ecoregion lables to sites.

--- bbsAnalysis.R
+++ bbsAnalysis.R
@@ -1,67 +1,72 @@
 library(tidyverse)
+library(FD)
+library(spData)
+library(sf)
 
-bird_trait <- read_csv("~/Documents/Projects/functional_diversity/data/elton_traits/elton_traits_BirdFuncDat.csv")
-bbs <- read_csv("data/bbs_data.csv")
-
-#join bbs and traits on scientific name to find taxonomic mismatches, and get a dataframe of taxonomic equivalancies based on common names
-sci_equivalent <- bbs %>% 
-  select(scientific, common_name) %>%
-  unique() %>%
-  left_join(select(bird_trait, scientific, english), by = "scientific") %>% #join on scientific name
-  subset(is.na(english)) %>% #get rows where the scientic names didn't match
-  select(scientific, common_name) %>% #select bbs sci name and common name
-  left_join(select(bird_trait, scientific, english), 
-            by = c("common_name" = "english")) %>% #join bbs and trait data on common name to see taxanomic equivalents
-  rename(bbs_sci = scientific.x, trait_sci = scientific.y) 
-                                                                                        
-
-## Yellow-rumped warbler = yellow-rumped warbler w/o commentary
-## Black-throated Gray Warbler = Black-throated Grey Warbler
-## Gray Hawk = grey hawk (technically different subspecies?)
-
-###No trait data for: Pacific Wren, Eastern Yellow Wagtail, Sagebrush sparrow, Woodhouse's scrub jay, Bell's sparrow
-
-
-get_compatible_sci_names <- function(data, sci_equiv){
-  #Create a new column called compat_sci that replaces BBS sci names with their trait data equivalent#
+trait <- read_csv("data/elton_traits/elton_traits_BirdFuncDat.csv")
+bbs <- read_csv("data/bbs_data.csv") #422 species
+bbs_trait <- read_csv("data/bbsTraits_master.csv") #414 species
   
-  #case when formula - one for each row of equivalencies 
-  replace_form <- lapply(1:dim(sci_equiv)[1],function(var) 
-    formula(paste0('data$scientific == as.character(sci_equiv$bbs_sci[',
-                   var, ']) ~ as.character(sci_equiv$trait_sci[', var,'])')))
-  
-  #add special cases where neither common or scientific names match, but the species are the same
-  ##still don't work, not sure why
-  replace_form <- append(replace_form, 
-                         c(formula('data$scientific == \'Setophaga coronata\' ~ \'Dendroica coronata\''),
-                           formula('data$scientific == \'Setophaga nigrescens\' ~ \'Dendroica nigrescens\''),
-                           formula('data$scientific == as.character(\'Buteo plagiatus\') ~ \'Buteo nitidus\'')
-                           )
-                         )
-  
-  #add case for when there is no equivalence and we keep the original name
-  replace_form <- append(replace_form, formula(paste0("TRUE ~ as.character(data$scientific)")))
-  
-  #add column
-  data %>%
-    mutate(compat_sci = case_when(!!!replace_form))
-  
+#Species Matrix  
+species <- bbs_trait %>% #401 species - where are they going??
+  filter(year > 2006) %>%
+  select(scientific, site_id, abundance) %>%
+  group_by(scientific, site_id) %>%
+  summarize(m = mean(abundance)) %>%
+  spread(scientific, m) %>%
+  column_to_rownames(var = "site_id")
+
+#Trait Matrix  
+traits <- trait %>%
+  filter(scientific %in% colnames(species)) %>%
+  select(-specid, -passnonpass, -iocorder, -blfamilylatin, -blfamilyenglish, -blfamsequid, -taxo, -bodymass_speclevel, -english, -diet_certainty,
+         -ends_with("source"), -ends_with("comment"), -ends_with("enteredby")) %>%
+  arrange(scientific) %>%
+  column_to_rownames(var = "scientific")
+
+#Get Functional Diversity Metrics  
+testFD <- dbFD(traits, species, w.abun = TRUE)
+save(testFD, file = "FD_stats.RData")
+
+
+#Get Ecoregion for each site
+p <-  4326 # +proj=longlat +datum=WGS84
+bcr <- st_read("data/bcr_shp/BCR_Terrestrial_master.shp") %>%
+  st_transform(crs = p) %>%
+  filter(REGION %in% c("CANADA", "USA"))
+
+get_route_data <- function(){
+  route_locations <- unique(dplyr::select(bbs, site_id, long, lat))
+  spatial_routes <- route_locations %>%
+    #dplyr::select(long, lat) %>%
+    st_as_sf(coords = c("long", "lat"), crs = p)
 }
 
-#test case
-test_bbs <- data.frame(scientific = c("a", "b", "c", "d"), abundance = c(1,2,3,4))
-test_sci <- data.frame(bbs_sci = c("a", "b"), trait_sci = c("a1", "b1"))
-get_compatible_sci_names(data = test_bbs, sci_equiv = test_sci)
+bbs_routes <- get_route_data()
 
-#get equivalence column for BBS data
-bbs_compat <- get_compatible_sci_names(bbs, sci_equivalent)
+bcr_names <- unique(bcr$BCRNAME)
+region_sites <- matrix(ncol = length(bcr_names), nrow = dim(bbs_routes)[1])
 
-
-#Merge bbs
-bbs_trait <- bird_trait %>%
-  select(-specid, -passnonpass, -iocorder, -blfamilylatin, -blfamilyenglish, -blfamsequid, -taxo, -bodymass_speclevel, -english,
-         -ends_with("source"), -ends_with("comment"), -ends_with("enteredby")) %>%
-  right_join(select(bbs_compat, -scientific), by = c("scientific" = "compat_sci")) %>%
-  filter(!is.na(scientific))
+for (i in 1:length(bcr_names)){
+  reg_name <- bcr %>%
+    filter(BCRNAME == bcr_names[i])
+  int_mat <- st_intersects(reg_name, bbs_routes, sparse = FALSE)
   
+  if (dim(int_mat)[1] > 0 & sum(int_mat > 0)){
+    print(1)
+    int_mat <- as.logical(colSums(int_mat))
+  }else{
+    print(c(2, i))
+    int_mat <- rep(FALSE, dim(bbs_routes)[1])
+  }
+  print(dim(int_mat))
+  region_sites[,i] <- (int_mat)
+  region_sites <- as.data.frame(region_sites)
+  colnames(region_sites) <- bcr_names
+}
+
+site_region_num <- apply(region_sites, 1, function(x) min(which(x == TRUE)))
+
+site_region_num_min <- apply(region_sites, 1, function(x) max(which(x == TRUE)))
+
 
