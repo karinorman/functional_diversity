e4a0660f5027c5b4b6c67225ce597fa8283f17c3
Norman <kn0@macwf03125.ornl.gov>
2018-06-08 14:51:04 -0400

    Clean up so the whole sscript can be run top to bottom. Fix bug in FD and site/region merge.

--- bbsAnalysis.R
+++ bbsAnalysis.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(dplyr)
 library(FD)
 library(spData)
 library(sf)
@@ -6,15 +7,18 @@ library(tmap)
 
 trait <- read_csv("data/elton_traits/elton_traits_BirdFuncDat.csv")
 bbs <- read_csv("data/bbs_data.csv") #422 species
-bbs_trait <- read_csv("data/bbsTraits_master.csv") #414 species
-  
+bbs_trait <- read_csv("data/bbsTraits_master.csv") #414 species, 8 species not merged with trait data, see importData.R for which species and why
+
+min_year = 2006 #define the minimum year of sampling to include
+p <-  4326 # +proj=longlat +datum=WGS84: Projection for spatial analyses
+
 #Species Matrix  
 get_species_matrix <- function(){
-  species <- bbs_trait %>% #401 species - where are they going??
-    filter(year > 2006) %>%
-    select(scientific, site_id, abundance) %>%
+  species <- bbs_trait %>% #401 species, 4176 sites, some not seen in the time period
+    filter(year > min_year) %>%
+    dplyr::select(scientific, site_id, abundance) %>%
     group_by(scientific, site_id) %>%
-    summarize(m = mean(abundance)) %>%
+    summarize(m = mean(abundance)) %>% #think about something other than mean?
     spread(scientific, m) %>%
     column_to_rownames(var = "site_id")
 }
@@ -23,7 +27,7 @@ get_species_matrix <- function(){
 get_trait_matrix <- function(){
   traits <- trait %>%
     filter(scientific %in% colnames(species)) %>%
-    select(-specid, -passnonpass, -iocorder, -blfamilylatin, -blfamilyenglish, -blfamsequid, -taxo, -bodymass_speclevel, -english, -diet_certainty,
+    dplyr::select(-specid, -passnonpass, -iocorder, -blfamilylatin, -blfamilyenglish, -blfamsequid, -taxo, -bodymass_speclevel, -english, -diet_certainty,
            -ends_with("source"), -ends_with("comment"), -ends_with("enteredby")) %>%
     arrange(scientific) %>%
     column_to_rownames(var = "scientific")
@@ -37,29 +41,28 @@ get_site_FD <- function(){
     FD_file <- load(data_path)
     return(as.data.frame(get(FD_file)))
   }else{
-    traits <- get_trait_matrix()
     species <- get_species_matrix()
-    
+    traits <- get_trait_matrix()
+
     FD <- dbFD(traits, species, w.abun = TRUE)
     save(FD, file = "FD_stats.RData")
   }
 }
 
-FD <- get_site_FD()
-
 #Get Ecoregions for each site
 
 get_ecoreg_shp <- function(){
-  p <-  4326 # +proj=longlat +datum=WGS84
   bcr <- st_read("data/bcr_shp/BCR_Terrestrial_master.shp") %>%
     st_transform(crs = p) %>%
     filter(REGION %in% c("CANADA", "USA"))
 }
 
-#get bbs sites as a spatial dataframe
+#get bbs sites for our time period as a spatial dataframe
 get_sites_sf <- function(){
-  route_locations <- unique(dplyr::select(bbs, site_id, long, lat))
-  spatial_routes <- route_locations %>%
+  spatial_routes <- bbs_trait %>%
+    filter(year > min_year) %>%
+    dplyr::select(site_id, long, lat) %>%
+    unique() %>%
     st_as_sf(coords = c("long", "lat"), crs = p)
 }
 
@@ -67,10 +70,9 @@ get_sites_sf <- function(){
 get_sites_w_region_FD <- function(){
   bcr <- get_ecoreg_shp()
   bbs_sites <- get_sites_sf()
-  
   bcr_names <- unique(bcr$BCRNAME)
-  region_sites <- matrix(ncol = length(bcr_names), nrow = dim(bbs_sites)[1])
   
+  region_sites <- matrix(ncol = length(bcr_names), nrow = dim(bbs_sites)[1])
   for (i in 1:length(bcr_names)){
     reg_name <- bcr %>%
       filter(BCRNAME == bcr_names[i])
@@ -110,21 +112,21 @@ get_sites_w_region_FD <- function(){
     mutate(site_id = as.integer(rowname)) %>%
     dplyr::select(-rowname)
   
-  region_sites[region_sites == 0 ] <- NA
+  region_sites[region_sites == FALSE ] <- NA
   site_labels <- region_sites %>% 
     gather(region, value, -site_id) %>% 
     na.omit() %>% 
     dplyr::select(-value) %>%
     left_join(., dplyr::select(region_sites, site_id)) %>%
     left_join(., bbs_sites) %>%
-    left_join(., rownames_to_column(FD), by = "site_id") %>%
+    left_join(., FD, by = "site_id") %>%
     arrange(site_id)
   ## ^^^ above joins result in multiple regions for each site - not sure what's going on 
 }
 
 bbs_site_FD <- get_sites_w_region_FD()
 
-sites_in_region <- bbs_sites %>% 
+sites_in_region <- bbs_site_FD %>% 
   group_by(region) %>%
   summarise(n = n()) %>%
   arrange(n)
@@ -133,12 +135,14 @@ sites_in_region <- bbs_sites %>%
 #Map dropped sites
 bbs_sites <- get_sites_sf()
 
-dropped_site_ids <- setdiff(bbs_sites$site_id, bbs_sites$site_id)
+dropped_site_ids <- dplyr::setdiff(bbs_sites$site_id, bbs_site_FD$site_id)
 dropped_sites <- bbs_sites %>% filter(site_id %in% dropped_site_ids)
 
 bcr <- get_ecoreg_shp()
 map_dropped = tm_shape(bcr) + tm_borders()
 map_dropped + tm_shape(dropped_sites) + tm_dots(col = "red")
+
+
 
 
 
